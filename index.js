9d0ba605f974e97473a1013dd3a4de3c5181e0f3
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-04-30 19:24:01 -0700

    Add redis to server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const newrelic = require('newrelic');
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const Stock = require('./database-mongodb/Stock.js');
-const Purchase = require('./database-mongodb/Stock2.js');
+const pool = require('./postgresql/index.js');
+const redis = require('redis');
+const client = redis.createClient();
+
 
 const app = express();
 const PORT = 3001;
@@ -13,42 +17,83 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, 'client/dist')));
 app.use('/stocks/:stockid', express.static(path.join(__dirname, 'client/dist')));
 
-app.get('/api/ratings', (req, res) => {
-  Stock.find({})
-    .exec((err, stockRatings) => { 
-      if (err) {
-        res.status(500).send('could not find any expert ratings of stocks');
-        throw (err);
-      }
-      res.status(200).send(stockRatings);
-    });
-});
 
-app.get('/api/ratings/:stockID', (req, res) => {
-  Stock
-    .find({ symbol: req.params.stockID.toUpperCase() })
-    .exec((err, data) => {
-      if (err) {
-        console.log(`Error: ${err}`);
-        res.status(500).send(err);
-        throw (err);
-      }
-      res.status(200).send(data);
-    });
-});
+// app.get('/api/ratings/:stockID', (req, res) => {
+//   var stock = req.params.stockID.toUpperCase();
+//   var stockRating = `SELECT * FROM analyst_ratings WHERE symbol = '${stock}'`;
+//   pool.query(stockRating, (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     res.status(200).json(results.rows);
+//   });
+// });
 
-app.get('/api/history/:stockID', (req, res) => {
-  Purchase
-    .find({ symbol: req.params.stockID.toUpperCase() })
-    .exec((err, data) => {
-      if (err) {
-        console.log(`Error: ${err}`);
-        res.status(500).send(err);
-        throw (err);
-      }
-      res.status(200).send(data);
-    });
-});
+const getRatings = (req, res, isbn) => {
+  var stock = req.params.stockID.toUpperCase();
+  var stockRating = `SELECT * FROM analyst_ratings WHERE symbol = '${stock}'`;
+  pool.query(stockRating)
+  .then((results) => {
+    client.setex(isbn, 3600, JSON.stringify(results.rows));
+    res.status(200).json(results.rows);
+  })
+  .catch(err => console.log(err))
+}
+
+
+const getCache = (req, res) => {
+  let isbn = req.params.stockID.toUpperCase();
+  //Check the cache data from the server redis
+  client.get(isbn, (err, result) => {
+    if (result) {
+      console.log('redis is working!')
+      res.send(result);
+    } else {
+      getRatings(req, res, isbn);
+    }
+  });
+}
+
+app.get('/api/ratings/:stockID', getCache);
+
+
+// app.get('/api/history/:stockID', (req, res) => {
+//   var stock = req.params.stockID.toUpperCase();
+//   var stockRating = `SELECT * FROM buying_history WHERE symbol = '${stock}'`;
+//   pool.query(stockRating, (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     res.status(200).json(results.rows);
+//   });
+// });
+
+const getBuyingHistory = (req, res, isbn) => {
+  var stock = req.params.stockID.toUpperCase();
+  var stockRating = `SELECT * FROM buying_history WHERE symbol = '${stock}'`;
+  pool.query(stockRating)
+  .then((results) => {
+    client.setex(isbn, 3600, JSON.stringify(results.rows));
+    res.status(200).json(results.rows);
+  })
+  .catch(err => console.log(err));
+};
+
+
+const getCache2 = (req, res) => {
+  let isbn = req.params.stockID.toUpperCase();
+  //Check the cache data from the server redis
+  client.get(isbn, (err, result) => {
+    if (result) {
+      console.log('redis2 is working!')
+      res.send(result);
+    } else {
+      getBuyingHistory(req, res, isbn);
+    }
+  });
+}
+
+app.get('/api/history/:stockID', getCache2);
 
 const server = app.listen(PORT, () => {
   console.log(`server running at: http://localhost:${PORT}`);
